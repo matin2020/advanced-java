63ad0070ad3741c9594982daf3b8a3d23710dc5a
mona <matin2020@hotmail.com>
2020-07-05 01:49:06 -0400

    Changed to hello world 2

--- Sample/src/app/App.java
+++ Sample/src/app/App.java
@@ -2,7 +2,7 @@ package app;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello World ,Now Using test");
+        System.out.println("Hello World 2 ,Now Using test");
         System.out.println("Abdul Matin choudhury");
         System.out.println("New commit after deleting new branch");
     }
